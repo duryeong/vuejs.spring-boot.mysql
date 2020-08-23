c60a49a1d919f884ba5f9fdce3b90089e268f0f7
duryeong <duryeong@gmail.com>
2020-08-24 00:34:58 +0900

    added src tests

---
+++ front-end/src/router.js
@@ -0,0 +1,15 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Home from '@/views/LoginPage.vue'
+
+Vue.use(Router)
+
+export default new Router({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes: [{
+  path: '/login',
+  name: 'LoginPage',
+  component: LoginPage
+  }]
+})
